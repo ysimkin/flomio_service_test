1469778692607d6fb5216145f60014ddd111980d
ysimkin <ysimkin@bpcreates.com>
2013-08-27 01:29:51 -0700

    fixed MainActivity to listen for broadcasts and toast

--- Flojack_Test1/src/com/bpcreates/flomio_service/MainActivity.java
+++ Flojack_Test1/src/com/bpcreates/flomio_service/MainActivity.java
@@ -1,16 +1,21 @@
 package com.bpcreates.flomio_service;
 
 
-import android.content.Intent;
+import java.util.Calendar;
 
 import android.app.Activity;
 import android.app.PendingIntent;
-import android.os.Bundle;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
 
-    
+	public static final String SM_BCAST_SCAN = "com.restock.serialmagic.gears.action.SCAN";
+	IntentFilter filter = new IntentFilter(SM_BCAST_SCAN);
     @Override
     protected void onResume() { 
         super.onResume();
@@ -22,6 +27,28 @@ public class MainActivity extends Activity {
         }catch(Exception e){
         	L.e("fail " + e);
         }
-        //finish();
-    }    
+        
+        registerReceiver(scanReceiver, filter);        
+    }  
+    
+    public void onPause(){
+    	super.onPause();
+    	unregisterReceiver(scanReceiver);
+    }
+    
+    
+    BroadcastReceiver scanReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			String s = intent.getAction();
+			if (s.equals(SM_BCAST_SCAN)) {
+				try{
+					String scan = intent.getStringExtra("scan");
+					Toast.makeText(MainActivity.this, "GOT SCAN " + scan, Toast.LENGTH_LONG).show();
+				}catch(Exception e){
+					Toast.makeText(MainActivity.this, "something went wrong: " + e, Toast.LENGTH_LONG).show();
+				}				
+			}			
+		}
+	};
 }
